41461f66ea69f2693c4cc82011530a734429cd1c
LuannMateus <luanmateuslima34@gmail.com>
2021-02-22 11:40:29 -0300

    Add React trail - first day

---
+++ moveit/src/App.tsx
@@ -0,0 +1,14 @@
+import ExperienceBar from "./components/ExperienceBar/ExperienceBar";
+
+import './styles/global.css';
+import './styles/App.css';
+
+function App() {
+  return (
+    <div className="container">
+      <ExperienceBar />
+    </div>
+  );
+}
+
+export default App;
